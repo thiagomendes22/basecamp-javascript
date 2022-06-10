6983aaea8fd91990288378263965274aa6fb48f1
Thiago Mendes <thiago.mds16@gmail.com>
2022-06-10 10:25:33 -0300

    restante do exercicio bootcampCarrefour

---
+++ map-filter-reduce/map/semthis.js
@@ -0,0 +1,11 @@
+function mapSemThis(arr) {
+ return arr.map(function (item) {
+   return item * 2;  
+ });   
+}
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array.map((item) => item * 2));
+
+console.log(array);
